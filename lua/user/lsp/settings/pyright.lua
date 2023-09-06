4521b572f27f99ee3c4865a887fb127fa6c465d5
Thomas Kirsch <kirsch.trier@gmail.com>
2023-09-06 14:33:10 +0200

    Add pyright configuration files

--- lua/user/lsp/settings/pyright.lua
+++ lua/user/lsp/settings/pyright.lua
@@ -3,8 +3,18 @@ return {
     python = {
       venvPath = ".",
       analysis = {
-        typeCheckingMode = "strict",
         diagnosticMode = "workspace",
+        -- https://github.com/microsoft/pyright/blob/main/docs/configuration.md#diagnostic-rule-defaults
+        diagnosticSeverityOverrides = {
+          "reportGeneralTypeIssues": "warning",
+          "reportMissingParameterType": "warning",
+          "reportUnknownArgumentType": "warning",
+          "reportUnknownLambdaType": "warning",
+          "reportUnknownMemberType": "warning",
+          "reportUnknownParameterType": "warning",
+          "reportUnknownVariableType": "warning",
+        },
+        typeCheckingMode = "strict",
       },
     },
   },
